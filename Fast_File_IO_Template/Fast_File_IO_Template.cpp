01094873b530c723ea13d74c12d3ecb1e397d6a5
Robert Tully <rtully95@gmail.com>
2020-01-29 10:00:16 -0600

    Updated c-style reader. Improved performance by performance of ~3 times over C++ ifstream / ofstream, at lower memory usage.

--- Fast_File_IO_Template/Fast_File_IO_Template.cpp
+++ Fast_File_IO_Template/Fast_File_IO_Template.cpp
@@ -1,4 +1,4 @@
-#ifdef _MSC_VER
+﻿#ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
@@ -7,29 +7,22 @@
 #include <fstream>
 #include <chrono> 
 
+#define NUM_FILE_DATA 2160000000
 #define DATATYPE_SIZE 2
 #define BUFFER_SIZE_MB 1
-#define NROWS 30000
-#define NCOLS 72000
 
 int main()
 {
 	auto start = std::chrono::high_resolution_clock::now();
 
-	FILE* CinFile = fopen("D:/Robert/Input_Data/Elevation/CONUS_ELEV_3_ARCSECONDS.bil", "r");;
-	FILE* CoutFile = fopen("D:/Robert/TEST/test_out.bil", "w+");
-	if (CinFile == NULL|| CoutFile == NULL)
-	{
-		std::cout << "Error opening files." << std::endl;
-		return -1;
-	}
+	FILE* CinFile = fopen("D:/Robert/Input_Data/Elevation/CONUS_ELEV_3_ARCSECONDS.bil", "rb");;
+	FILE* CoutFile = fopen("D:/Robert/TEST/test_out.bil", "w+b");
 
-	// Set up File Streams
-	// std::ifstream inFile("D:/Robert/Input_Data/Elevation/CONUS_ELEV_3_ARCSECONDS.bil", std::ios::in | std::ios::binary);
-	// std::ofstream outFile("D:/Robert/TEST/test_out.bil", std::ios::out | std::ios::binary);
+	if (CinFile == NULL || CoutFile == NULL)
+		return -1;
 
 	// Buffer and File Size
-	long long int file_bytes = ((long long int)NROWS) * ((long long int)NCOLS) * ((long long int)DATATYPE_SIZE); // bytes
+	long long int file_bytes = (long long int)NUM_FILE_DATA * (long long int)DATATYPE_SIZE;
 	long long int buffer_bytes = ((long long int)1024) * ((long long int)1024) * BUFFER_SIZE_MB;
 
 	// Calculate Remainder
@@ -43,15 +36,14 @@ int main()
 	// Read all data and Write Data
 	int num_iterations = (int)ceil((double)file_bytes / (double)buffer_bytes);
 	long long int read_size = buffer_bytes;
+
 	for (int i = 0; i < num_iterations; i++) {
 
-
-		// On the last iteration, the read size changes to the 
-		// number of bytes remaining in the file.
+		// On last iteration, read_size changes to the bytes remaining in the file.
 		if (i == num_iterations - 1)
 			read_size = remainder_bytes;
 
-		fread(buffer, (size_t)DATATYPE_SIZE, (size_t)read_size/ (size_t)DATATYPE_SIZE, CinFile);
+		fread(buffer, (size_t)DATATYPE_SIZE, (size_t)read_size / (size_t)DATATYPE_SIZE, CinFile);
 		///////////////////////
 		// do something here //
 		///////////////////////
