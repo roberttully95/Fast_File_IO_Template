f0b774068d47142142ca77c1d4b0667f20095a8f
Robert Tully <rtully95@gmail.com>
2020-01-28 22:53:33 -0600

    Just a few small changes to tidy things up. Less code, only one read / write block.

--- Fast_File_IO_Template/Fast_File_IO_Template.cpp
+++ Fast_File_IO_Template/Fast_File_IO_Template.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <chrono> 
 
 #define DATATYPE_SIZE 2
-#define BUFFER_SIZE_MB 40
+#define BUFFER_SIZE_MB 1
+#define NROWS 30000
+#define NCOLS 72000
 
 int main()
 {
+	auto start = std::chrono::high_resolution_clock::now();
+
 	// Set up File Streams
 	std::ifstream inFile("D:/Robert/Input_Data/Elevation/CONUS_ELEV_3_ARCSECONDS.bil", std::ios::in | std::ios::binary);
 	std::ofstream outFile("D:/Robert/TEST/test_out.bil", std::ios::out | std::ios::binary);
 
 	// Buffer and File Size
-	long long int file_data = ((long long int)30000) * ((long long int)72000); // not in bytes
-	long long int file_bytes = file_data * ((long long int)DATATYPE_SIZE); // bytes
+	long long int file_bytes = ((long long int)NROWS) * ((long long int)NCOLS) * ((long long int)DATATYPE_SIZE); // bytes
 	long long int buffer_bytes = ((long long int)1024) * ((long long int)1024) * BUFFER_SIZE_MB;
-	long long int buffer_data = buffer_bytes / ((long long int)DATATYPE_SIZE);
 
 	// Calculate Remainder
-	long long int remainder_data = file_data % buffer_data;
-	long long int remainder_bytes = remainder_data * ((long long int)DATATYPE_SIZE);
-
-	// Determine whether the final read of the data needs to be truncated.
-	bool has_remainder = true;
-	if (remainder_bytes == 0) {
-		has_remainder = false;
-	}
+	long long int remainder_bytes = file_bytes % buffer_bytes;
+	if (remainder_bytes == 0)
+		remainder_bytes = buffer_bytes;
 
 	// Create Buffer
-	unsigned short* buffer = new unsigned short[buffer_data];
+	unsigned short* buffer = new unsigned short[buffer_bytes/DATATYPE_SIZE];
 
-	// Determine the number of iterations that need to be made to traverse entire dataset.
+	// Read all data and Write Data
 	int num_iterations = (int)ceil((double)file_bytes / (double)buffer_bytes);
+	long long int read_size = buffer_bytes;
 	for (int i = 0; i < num_iterations; i++) {
 
-		// Remainder present, do truncated read
-		if (i == num_iterations - 1 && has_remainder == true) {
-			inFile.read(reinterpret_cast<char*>(buffer), (size_t)remainder_bytes);
-			outFile.write(reinterpret_cast<char*>(buffer), (size_t)remainder_bytes);
-			break;
-		}
+		// On the last iteration, the read size changes to the 
+		// number of bytes remaining in the file.
+		if (i == num_iterations - 1)
+			read_size = remainder_bytes;
 
-		// Read the entire buffer. This will always be the case until perhaps
-		// the final iteration in which we require to perform a truncated read.
-		inFile.read(reinterpret_cast<char*>(buffer), (size_t)buffer_bytes);
-		outFile.write(reinterpret_cast<char*>(buffer), (size_t)buffer_bytes);
+		inFile.read(reinterpret_cast<char*>(buffer), (size_t)read_size);
+		///////////////////////
+		// do something here //
+		///////////////////////
+		outFile.write(reinterpret_cast<char*>(buffer), (size_t)read_size);
 	}
 
 	// Close File Pointers
 	inFile.close();
 	outFile.close();
+
+	// Benchmark
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+	std::cout << "Time Taken = " << duration.count() << " seconds." << std::endl;
 }
